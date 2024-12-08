ee8e3ac46dbdea5117448f59f46516a54a5b3fef
Pavel Krasovsky <frostcat2001@gmail.com>
2024-12-08 18:53:48 +0300

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-    return <div className=""></div>;
+import {prisma} from '@/shared/lib/db';
+import {Card, CardTitle} from '@/shared/ui/card';
+
+export default async function Home() {
+    const games = await prisma.game.findMany();
+
+    console.log('games', games);
+
+    return (
+        <div className="">
+            {games.map((game) => {
+                return (
+                    <Card key={game.id}>
+                        <CardTitle>{game.name}</CardTitle>
+                    </Card>
+                );
+            })}
+        </div>
+    );
 }
